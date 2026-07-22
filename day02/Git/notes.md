ccda78fba19cdaa722aac4dd357c0f192d5c9953
Yohannes Belete <130431688+yemijohnnyaladel@users.noreply.github.com>
2026-07-22 16:30:56 +0300

    update notes and ad day02 python exercise

---
+++ day02/Git/notes.md
@@ -0,0 +1,1 @@
+this is a day 02 project onm ibt collegue on python
---
+++ day02/everyone.py
@@ -0,0 +1,8 @@
+student_name = "Alma Bekele"  #str
+age = 24    #int
+balance = 1500.50  #float(ETB)
+is_enrolled  = True   #bool
+verified = None   # no value yet
+age_text = input("your age") # e.g "24"(a string!)
+age = int(age_text)   #24 (now an int)
+next_year = age+1     # works only after int()21
